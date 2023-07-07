231bb285f09d5e4405d4b2080b5195c8b8615c9a
Alexandre Blanché <blanchealexandre11@yahoo.fr>
2023-07-07 13:18:47 +0200

    Bresenham complete, debug, plotoff, out_of_screen

--- picture_editor.ml
+++ picture_editor.ml
@@ -12,16 +12,7 @@ let size = 7;; (* Size of a pixel of the screen *)
 let width = 128*size;;
 let height = 64*size;;
 
-let marginf = float_of_int margin;;
-let sizef = float_of_int size;;
-let widthf = float_of_int width;;
-let heightf = float_of_int height;;
-
-(* Geometric vector *)
-(* (x,y)
-	x belongs to [0.,widthf[
-	y belongs to [0.,heightf[ *)
-type vector = float * float;;
+let gray = rgb 120 120 148;;
 
 (* Cell of the screen:
 	 (i,j)
@@ -29,43 +20,13 @@ type vector = float * float;;
 	 j belongs to 0..63 *)
 type cell = int * int;;
 
-(* Returns the vector ab = b - a *)
-let vect (a : vector) (b : vector) : vector =
-	let (xa,ya) = a
-	and (xb,yb) = b in
-	(xb-.xa, yb-.ya);;
-
-(* Returns the vector at the center of the cell (i,j) *)
-let cell_to_vector (c : cell) : vector =
-	let (i,j) = c in
-	(float_of_int (margin+size*i)+.(sizef/.2.), float_of_int (margin+size*j)+.(sizef/.2.));;
+exception Out_of_screen
 
 (* Returns the cell that contains the vector (x,y) *)
-let vector_to_cell (x : int) (y : int) : cell =
-	if x<marginf || x>marginf+.widthf || y<marginf || y>marginf+.heightf
-		then (0,0)
+let coord_to_cell (x : int) (y : int) : cell =
+	if x<margin || x>margin+width || y<margin || y>margin+height
+		then raise Out_of_screen
 		else ((x-margin)/size, ((y-margin)/size));;
-
-(* Returns the vector v scaled by factor scal *)
-let ( // ) (v : vector) (scal : float) =
-	let (x,y) = v in
-	(scal*.x, scal*.y);;
-
-(* Returns the norm of vector v *)
-let norm (v : vector) : float =
-	let (x,y) = v in
-	sqrt ((x*.x)+.(y*.y));;
-
-(* Returns the vector v applied to point pt *)
-let extr (pt : vector) (v : vector) : vector =
-	let (a,b) = pt
-	and (x,y) = v in
-	(a+.x, b+.y);;
-
-(* Returns a unit vector colinear with v *)
-let unit_vect v =
-	let n = norm v in
-	v // (1./.n);;
 
 (* Setup functions *)
 let cache () =
@@ -89,144 +50,42 @@ let dline (a : int) (b : int) (x : int) (y : int) =
 	lineto x y;;
 
 (* Fills the pixel x,y of the screen with the current color *)
-let plot (i : int) (j : int) =
-	fill_rect (margin+size*i) (margin+size*j) size size;;
+let plot (m : bool array array) (i : int) (j : int) =
+	fill_rect (margin+size*i) (margin+size*j) size size;
+	m.(j).(i) <- true;;
+
+(* Fills the pixel x,y of the screen with white and reforms the grid *)
+let plotoff (m : bool array array) (grid : bool) (i : int) (j : int) =
+	if m.(j).(i) then
+		(set_color white;
+		fill_rect (margin+size*i) (margin+size*j) size size;
+		m.(j).(i) <- false;
+		if grid then
+			(* reform the grid around the deleted pixel *)
+			(set_color gray;
+			rect (margin+size*i) (margin+size*j) size size;
+			set_color black));;
+
 
 (* Traces an horizontal line between pixel (i1,j) and (i2,j) (i1 is the leftmost pixel) *)
-let horitzontal_line (i1 : int) (i2 : int) (j : int) =
-	fill_rect (margin+size*i1) (margin+size*j) ((i2-i1+1)*size) size;;
+let horitzontal_line (m : bool array array) (i1 : int) (i2 : int) (j : int) =
+	fill_rect (margin+size*i1) (margin+size*j) ((i2-i1+1)*size) size;
+	for i = i1 to i2 do
+		m.(j).(i) <- true
+	done;;
 
 (* Traces a vertical line between pixel (i,j1) and (i,j2) (j1 is the lowest pixel) *)
-let vertical_line (i : int) (j1 : int) (j2 : int) =
-	fill_rect (margin+size*i) (margin+size*j1) size ((j2-j1+1)*size);;
-
-(* Obsolete *)
-(* (* Auxiliary function to the function f_line
-	m is the pixel matrix,
-	i,j is the current position
-	k,l is the destination position *)
-let rec f_line_aux m i j k l =
-	
-	(*print_string "f_line : x,y = ";
-	print_int x; print_string ","; print_int y;
-	print_string " - a,b = ";
-	print_int a; print_string ","; print_int b;
-	print_newline();*)
-	
-	let ptij = cell_to_vector (i,j) in
-	let ptkl = cell_to_vector (k,l) in
-	let v = vect ptij ptkl in
-	let v' = (unit_vect v) // 16. in
-	let vbis = (unit_vect v) // 20. in
-
-	let x,y = ptij in
-	let pt = extr ptij v' in
-	let ptbis = extr ptij vbis in
-	
-	let (a,b) = ptbis in
-	let (c,d) = vector_to_cell pt in
-	
-	m.(d).(c) <- true;
-	plot c d;
-		
-	set_color red;
-	dline (int_of_float x) (int_of_float y) (int_of_float a) (int_of_float b);
-	set_color black;
-		
-	if c<>k || d<>l then
-		f_line_aux m c d k l;;
-
-(* Traces a line between pixel i,j and k,l
-	m is the pixel matrix *)
-let f_line m i j k l =
-	cache();
-	plot i j;
-	m.(j).(i) <- true;
-	f_line_aux m i j k l;
-	sync();;
-
-
-let f_linebis m x y a b =
-	cache();
-	let ptxy = cell_to_vector (x,y) in
-	let v = vect ptxy (cell_to_vector (a,b)) in
-	let k = int_of_float ((norm v)/.7.) in
-	for i=0 to k do
-	let p = float_of_int i in
-		let (a,b) = extr ptxy (v//(p/.(float_of_int k))) in
-		let (c,d) = vector_to_cell (a,b) in
-		m.(d).(c) <- true;
-		plot c d;
-	done;
-	sync();; *)
-
-(* Traces a line between pixel (i1,j1) and pixel (i2,j2)
-	through the Bresenham algorithm in the eight octants
-	+ 2 horizontals + 2 verticals *)
-let bresenham (i1 : int) (j1 : int) (i2 : int) (j2 : int) =
-	let di = i2-i1 in
-	let dj = j2 - j1 in
-	let e = ref di in
-	if di <> 0 then
-		if di > 0 then
-			if dj <> 0 then
-				if dj > 0 then (* dj > 0, di > 0, 1st quadrant *)
-					if di >= dj then (* 1st octant *)
-						let ddi = 2*di in
-						let ddj = 2*dj in
-						let j = ref j1 in
-						for i = i1 to i2 do
-							plot i !j;
-							e := !e - ddj;
-							if !e < 0 then
-								(j := !j + 1;
-								e:= !e + ddi);
-						done
-					else (* 2nd octant *)
-						()
-				else (* dj < 0, di > 0, 4th quadrant *)
-					if di >= -dj then (* 8th octant *)
-						()
-					else (* 7th octant *)
-						()
-			else (* dj = 0, di > 0, horizontal to the right *)
-				for i = i1 to i2 do
-					plot i j1
-				done
-		else (* di < 0 *)
-			if dj <> 0 then
-				if dj > 0 then (* 2nd quadrant *)
-					if -di > dj then (* 4th octant *)
-						()
-					else (* 3rd octant *)
-						()
-				else (* dj < 0, di < 0, 3rd quadrant *)
-					if di <= dj then (* 5th octant *)
-						()
-					else (* 6th octant *)
-						()
-			else (* dj = 0, di < 0, horizontal to the left *)
-				for i = i2 to i1 do
-					plot i j1
-				done
-	else (* di = 0 *)
-		if dj <> 0 then
-			if dj > 0 then (* vertical, to the top *)
-				for j = j1 to j2 do
-					plot i1 j
-				done
-			else (* vertical, to the bottom *)
-				for j = j2 to j1 do
-					plot i1 j
-				done
-		else (* i1=i2, j1=j2 *)
-			plot i1 j1;;
+let vertical_line (m : bool array array) (i : int) (j1 : int) (j2 : int) =
+	fill_rect (margin+size*i) (margin+size*j1) size ((j2-j1+1)*size);
+	for j = j1 to j2 do
+		m.(j).(i) <- true
+	done;;
 
 
 (* Bresenham algorithm *)
 
 (* 1st and 8th octants *)
-let bresenham_loop_18 (i1 : int) (j1 : int) (i2 : int) (j2 : int) =
+let bresenham_loop_18 (m : bool array array) (i1 : int) (j1 : int) (i2 : int) (j2 : int) =
 	let di = i2 - i1 in
 	let e = ref di in (* e>0 *)
 	let ddi = 2*di in
@@ -248,19 +107,19 @@ let bresenham_loop_18 (i1 : int) (j1 : int) (i2 : int) (j2 : int) =
 	while !i < i2 do
 		ibeg := !i;
 		e := !e - sdj*ddj;
-		while !e>=0 || !i=i2-1 do
+		while !e>=0 && !i<>i2-1 do
 			e := !e - sdj*ddj;
 			incr i
 		done;
-		horitzontal_line !ibeg !i !j;
+		horitzontal_line m !ibeg !i !j;
 		j := !j + sdj;
 		e:= !e + ddi;
 		incr i
 	done;
-	plot i2 j2;;
+	plot m i2 j2;;
 
 (* 4th and 5th octants *)
-let bresenham_loop_45 (i1 : int) (j1 : int) (i2 : int) (j2 : int) =
+let bresenham_loop_45 (m : bool array array) (i1 : int) (j1 : int) (i2 : int) (j2 : int) =
 	let di = i2 - i1 in
 	let e = ref di in (* e<0 *)
 	let ddi = 2*di in
@@ -276,23 +135,24 @@ let bresenham_loop_45 (i1 : int) (j1 : int) (i2 : int) (j2 : int) =
 			(j := !j + sdj;
 			e:= !e + ddi);
 	done; *)
+	let i = ref i1 in
 	let ibeg = ref i1 in
-	while !i > i2+1 do
+	while !i > i2 do
 		ibeg := !i;
 		e := !e + sdj*ddj;
-		while !e<0 || !i=i2+1 do
+		while !e<0 && !i<>i2+1 do
 			e := !e + sdj*ddj;
 			decr i
 		done;
-		horitzontal_line !i !ibeg !j;
+		horitzontal_line m !i !ibeg !j;
 		j := !j + sdj;
 		e:= !e + ddi;
 		decr i
 	done;
-	plot i2 j2;;
+	plot m i2 j2;;
 
 (* 2nd and 3rd octants *)
-let bresenham_loop_23 (i1 : int) (j1 : int) (i2 : int) (j2 : int) =
+let bresenham_loop_23 (m : bool array array) (i1 : int) (j1 : int) (i2 : int) (j2 : int) =
 	let dj = j2 - j1 in
 	let e = ref dj in (* e>0 *)
 	let ddi = 2*(i2-i1) in
@@ -302,46 +162,67 @@ let bresenham_loop_23 (i1 : int) (j1 : int) (i2 : int) (j2 : int) =
 	let sdi = if ddi > 0 then 1 else -1 in
 	let i = ref i1 in
 	let j = ref j1 in
-	(* We trace the vertical segments in one call *)
 	let jbeg = ref j1 in
 	while !j < j2 do
 		jbeg := !j;
 		e := !e - sdi*ddi;
-		while !e>=0 || !j=j2-1 do
+		while !e>=0 && !j<>j2-1 do
 			e := !e - sdi*ddi;
 			incr j
 		done;
-		horitzontal_line !i !jbeg !j;
+		vertical_line m !i !jbeg !j;
 		i := !i + sdi;
 		e:= !e + ddj;
 		incr j
 	done;
-	plot i2 j2;;
+	plot m i2 j2;;
 
-let bresenham (i1 : int) (j1 : int) (i2 : int) (j2 : int) =
+(* 6th and 7th octants *)
+let bresenham_loop_67 (m : bool array array) (i1 : int) (j1 : int) (i2 : int) (j2 : int) =
+	let dj = j2 - j1 in
+	let e = ref dj in (* e<0 *)
+	let ddi = 2*(i2-i1) in
+	let ddj = 2*dj in
+	(* sdi = -1 : 6th octant
+		sdi = 1 : 7th octant *)
+	let sdi = if ddi > 0 then 1 else -1 in
+	let i = ref i1 in
+	let j = ref j1 in
+	let jbeg = ref j1 in
+	while !j > j2 do
+		jbeg := !j;
+		e := !e + sdi*ddi;
+		while !e<0 && !j<>j2+1 do
+			e := !e + sdi*ddi;
+			decr j
+		done;
+		vertical_line m !i !j !jbeg;
+		i := !i + sdi;
+		e:= !e + ddj;
+		decr j
+	done;
+	plot m i2 j2;;
+
+let bresenham (m : bool array array) (i1 : int) (j1 : int) (i2 : int) (j2 : int) =
 	let di = i2 - i1 in
 	let dj = j2 - j1 in
-	match (di=0),(di>0),(dj=0),(dj>0),(di>=dj),(di>=-dj),(-di>dj),(di<=dj) with
-		| true,_,true,_,_,_,_,_ -> plot i1 j1 (* single point *)
-		| true,_,_,true,_,_,_,_ -> vertical_line i1 j1 j2 (* vertical ascending *)
-		| true,_,_,_,_,_,_,_ -> vertical_line i1 j2 j1 (* vertical descending *)
-		| _,true,true,_,_,_,_,_ -> horitzontal_line i1 i2 j (* horizontal right *)
-		| _,true,_,true,true,_,_,_ -> bresenham_loop_18 i1 j1 i2 j2 (* 1st octant *)
-		| _,true,_,true,_,_,_,_ -> (* 2nd octant *)
-		| _,true,_,_,_,true,_,_ -> bresenham_loop_18 i1 j1 i2 j2 (* 8th octant *)
-		| _,true,_,_,_,_,_,_ -> (* 7th octant *)
-		| _,_,true,_,_,_,_,_ -> horitzontal_line i2 i1 j (* horitzontal left *)
-		| _,_,_,true,_,_,true,_ -> bresenham_loop_45 i1 j1 i2 j2 (* 4th octant *)
-		| _,_,_,true,_,_,_,_ -> (* 3rd octant *)
-		| _,_,_,_,_,_,_,true -> bresenham_loop_45 i1 j1 i2 j2 (* 5th octant *)
-		| _ -> (* 6th octant *);;
-
-
+	(match (di=0),(di>0),(dj=0),(dj>0),(di>=dj),(di>=(-dj)),(-di>dj),(di<=dj) with
+		| true,_,true,_,_,_,_,_ -> plot m i1 j1 (* single point *)
+		| true,_,_,true,_,_,_,_ -> vertical_line m i1 j1 j2 (* vertical ascending *)
+		| true,_,_,_,_,_,_,_ -> vertical_line m i1 j2 j1 (* vertical descending *)
+		| _,true,true,_,_,_,_,_ -> horitzontal_line m i1 i2 j1 (* horizontal right *)
+		| _,true,_,true,true,_,_,_ -> bresenham_loop_18 m i1 j1 i2 j2 (* 1st octant *)
+		| _,true,_,true,_,_,_,_ -> bresenham_loop_23 m i1 j1 i2 j2 (* 2nd octant *)
+		| _,true,_,_,_,true,_,_ -> bresenham_loop_18 m i1 j1 i2 j2 (* 8th octant *)
+		| _,true,_,_,_,_,_,_ -> bresenham_loop_67 m i1 j1 i2 j2 (* 7th octant *)
+		| _,_,true,_,_,_,_,_ -> horitzontal_line m i2 i1 j1 (* horitzontal left *)
+		| _,_,_,true,_,_,true,_ -> bresenham_loop_45 m i1 j1 i2 j2 (* 4th octant *)
+		| _,_,_,true,_,_,_,_ -> bresenham_loop_23 m i1 j1 i2 j2 (* 3rd octant *)
+		| _,_,_,_,_,_,_,true -> bresenham_loop_45 m i1 j1 i2 j2 (* 5th octant *)
+		| _ -> bresenham_loop_67 m i1 j1 i2 j2 (* 6th octant *));;
 
 
 (** Graphical interface **)
-
-let grey = rgb 120 120 148;;
 
 (* Opens and configure the graphical window *)
 let config (grid : bool) : unit =
@@ -357,7 +238,7 @@ let config (grid : bool) : unit =
 	(* grid *)
 	if grid
 		then
-			(set_color grey;
+			(set_color gray;
 			for i=1 to 127 do
 				dline (margin+size*i) margin (margin+size*i) (margin+height)
 			done;
@@ -394,37 +275,48 @@ let interface (grid : bool) : bool array array =
 				| None -> ()
 				| Some (i,j) ->
 					(a_pressed := None;
-					set_color white;
-					plot i j;
-					sync();
-					set_color black));
+					plotoff m grid i j;
+					sync ()));
 			
 			(* display of the point on which we clicked *)
-			let (i,j) = vector_to_cell mouse_x mouse_y in
-			plot i j;
-			sync();
-			m.(j).(i) <- true;
+			try
+				let (i,j) = coord_to_cell mouse_x mouse_y in
+				plot m i j;
+				sync()
+			with
+				| Out_of_screen -> ();
+			
 		end;
 		
-		if keypressed && key = 'a' then
-			match !a_pressed with
-				| None ->
-					begin
-						let (i,j) = vector_to_cell mouse_x mouse_y in
-						set_color red;
-						plot i j;
-						sync();
-						set_color black;
-						a_pressed := Some (i,j)
-					end
-				| Some (i,j) ->
-					begin
-						let k,l = vector_to_cell mouse_x mouse_y in
-						if k<>i || j<>l
-							then
-								(f_linebis m i j k l;
-								a_pressed := None)
-					end
+		if keypressed then
+			if key = 'a' then
+				match !a_pressed with
+					| None ->
+						(try
+							let (i,j) = coord_to_cell mouse_x mouse_y in
+							set_color red;
+							plot m i j;
+							sync ();
+							set_color black;
+							a_pressed := Some (i,j)
+						with
+							| Out_of_screen -> ())
+					| Some (i,j) ->
+							try
+								let (k,l) = coord_to_cell mouse_x mouse_y in
+								if k<>i || j<>l	then
+									(bresenham m i j k l;
+									sync();
+									a_pressed := None)
+							with
+								| Out_of_screen -> ()
+			else if key = 'd' then
+				try
+					let (i,j) = coord_to_cell mouse_x mouse_y in
+					plotoff m grid i j;
+					sync ()
+				with
+					| Out_of_screen -> ()
 	done;
 	close_graph ();
 	m;;
@@ -433,22 +325,16 @@ let print_mat (m : bool array array) : unit =
 	config false;
 	set_color black;
 	cache();
-	for i=0 to 63 do
-		for j=0 to 127 do
-			if m.(i).(j) then
-				plot j i;
+	for j=0 to 63 do
+		for i=0 to 127 do
+			if m.(j).(i) then
+				plot m i j;
 		done;
 	done;
 	sync();
 	let _ = wait_next_event [Button_down; Key_pressed] in
 	close_graph ();;
 
-(* Todo:
-	remove all useless casts to float
-	take care of plot off (white pixels leave a hole in the grid)
-	take care of out of bounds
-
-	implement
-	- Bresenham algorithm (to trace a line)
+(* implement
 	- Floyd-Steinberg algorithm (to convert to actual monochrome) *)
 
