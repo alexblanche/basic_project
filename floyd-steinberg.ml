e9b5e278a9cc3abe05c46d79b384bcf2489f0375
Alexandre Blanché <blanchealexandre11@yahoo.fr>
2023-07-19 21:42:41 +0200

    Threshold mode (a few bugs)

--- floyd-steinberg.ml
+++ floyd-steinberg.ml
@@ -5,10 +5,18 @@
 
 (* Returns true if the color (r,g,b) is closer to black (pixel)
    and false if it is closer to white (no pixel) *)
-(* Closer to black: (r+g+b)/3 <= 255*threshold
-   threshold = 0.5 by default*)
-let closest_color (r : int) (g : int) (b : int) (threshold : float) : bool =
-  r+g+b <= int_of_float(765.*.threshold);;
+let closest_color (r : int) (g : int) (b : int) : bool =
+  let rf = float_of_int r in
+  let gf = float_of_int g in
+  let bf = float_of_int b in
+  (0.3*.rf +. 0.59*.gf +. 0.11*.bf) <= 127.5;;
+
+(* Same with a threshold *)
+let closest_color_threshold (r : int) (g : int) (b : int) (threshold : float) : bool =
+  let rf = float_of_int r in
+  let gf = float_of_int g in
+  let bf = float_of_int b in
+  (0.3*.rf +. 0.59*.gf +. 0.11*.bf) <= 255. *. threshold;;
 
 (* Returns the tuple (r,g,b) associated with the color c *)
 let reverse_rgb (c : Graphics.color) : int * int * int =
@@ -89,16 +97,15 @@ let error_propagation (img : image_mat) (i : int) (j : int) (err_r : int) (err_g
   img.(j).(i) <- rgb (r+err_r*coef/16) (g+err_g*coef/16) (b+err_b*coef/16);;
 
 (* Returns a 64*128 matrix of monochromatic pixels (of type bool array array)
-  that approximates the image img through the Floyd-Steinberg algorithm *)
-(* We assume that width/height >= 2 *)
-let floyd_steinberg (img : image_mat) : bool array array =
-  let rimg = reduce_img img 64 128 in (* Resized image (with RGB colors) *)
+  that approximates the image rimg through the Floyd-Steinberg algorithm *)
+(* We assume that rimg is a reduced image of size 64*128 *)
+let floyd_steinberg (rimg : image_mat) : bool array array =
   let mono = Array.make_matrix 64 128 false in
   (* Treatment of the image, except the bottom line *)
   for j = 63 downto 1 do
     (* Leftmost line *)
     let (r,g,b) = reverse_rgb (rimg.(j).(0)) in
-    let cc = closest_color r g b 0.5 in
+    let cc = closest_color r g b in
     mono.(j).(0) <- cc;
     let c = (if cc then 0 else 255) in
     let err_r = r - c in
@@ -110,7 +117,7 @@ let floyd_steinberg (img : image_mat) : bool array array =
     for i = 1 to 126 do
       (* Interior of the image *)
       let (r,g,b) = reverse_rgb (rimg.(j).(i)) in
-      let cc = closest_color r g b 0.5 in
+      let cc = closest_color r g b in
       mono.(j).(i) <- cc;
       let c = (if cc then 0 else 255) in
       let err_r = r - c in
@@ -123,7 +130,7 @@ let floyd_steinberg (img : image_mat) : bool array array =
     done;
     (* Rightmost line *)
     let (r,g,b) = reverse_rgb (rimg.(j).(127)) in
-    let cc = closest_color r g b 0.5 in
+    let cc = closest_color r g b in
     mono.(j).(127) <- cc;
     let c = (if cc then 0 else 255) in
     let err_r = r - c in
@@ -135,7 +142,7 @@ let floyd_steinberg (img : image_mat) : bool array array =
   (* Treatment of the bottom line *)
   for i = 0 to 126 do
     let (r,g,b) = reverse_rgb (rimg.(0).(i)) in
-    let cc = closest_color r g b 0.5 in
+    let cc = closest_color r g b in
     mono.(0).(i) <- cc;
     let c = (if cc then 0 else 255) in
     let err_r = r - c in
@@ -145,7 +152,21 @@ let floyd_steinberg (img : image_mat) : bool array array =
   done;
   (* Bottom-right pixel *)
   let (r,g,b) = reverse_rgb (rimg.(0).(127)) in
-  mono.(0).(127) <- closest_color r g b 0.5;
+  mono.(0).(127) <- closest_color r g b;
+  mono;;
+
+(* Returns a 64*128 matrix of monochromatic pixels (of type bool array array)
+  that approximates the image rimg through the threshold algorithm:
+  each pixel (r,g,b) is set to white if (r+g+b)/3 < 255*threshold *)
+(* We assume that rimg is a reduced image of size 64*128 *)
+let img_to_mono_threshold (rimg : image_mat) (thresh : float) : bool array array =
+  let mono = Array.make_matrix 64 128 false in
+  for i = 0 to 127 do
+    for j = 0 to 63 do
+      let (r,g,b) = reverse_rgb (rimg.(j).(i)) in
+      mono.(j).(i) <- closest_color_threshold r g b thresh
+    done
+  done;
   mono;;
 
 (* Testing *)
@@ -155,43 +176,79 @@ let test_reduce (img : image_mat) =
   view_image rimg;;
 
 (* Displays the original image and its monochromatic conversion side by side *)
-let view_side_by_side (img : image_mat) (mono : bool array array) =
+let view_side_by_side (img : image_mat) =
+  let rimg = reduce_img img 64 128 in
+  let mono_fs = floyd_steinberg rimg in
   let height = Array.length img
   and width = Array.length img.(0) in
-  let margin = 20 in
-  let size = min (height/64) (width/128) in
-  let size_conv = max size 5 in
-  let img_mono = mono_to_image_mat mono size_conv in
+  let margin = 35 in
+  let size = max (min (height/64) (width/128)) 5 in
+  let img_mono_fs = mono_to_image_mat mono_fs size in
+  let exit = ref false in
+  let fs_mode = ref true in (* true if Floyd-Steinberg, false if Threshold mode *)
+  let thresh = ref 0.5 in
+  let mono_th = ref (img_to_mono_threshold rimg !thresh) in
   
   open_graph "";
-  resize_window (3*margin + width + size_conv*128) (2*margin + max height (size_conv*64));
+  resize_window (3*margin + width + size*128) (2*margin + max height (size*64));
   set_window_title "Monochromatic Conversion";
   auto_synchronize false;
-
   let pict_img = make_image img in
-  let pict_mono = make_image img_mono in
-  draw_image pict_img margin margin;
-  set_color black;
-  rect (2*margin + width) margin (128*size_conv + 1) (64*size_conv + 1);
-  draw_image pict_mono (2*margin + width + 1) (margin + 1);
-  set_color red;
-  (if width < height*2
-    then (* Ratio < 2:1 (portrait, 16/9...) *)
-      rect margin (margin + height - width/2) width (width/2)
-    else (* Ratio >= 2:1 (flat landscape) *)
-      rect margin margin (2*height) height); 
-  synchronize ();
-	
-  let _ = read_key() in
-  close_graph();;
+  let pict_mono_fs = make_image img_mono_fs in
+  let bg () =
+    set_color black;
+    moveto 0 15;
+    draw_string "[a] to toggle Floyd-Steinberg/Threshold mode, [Esc] to quit.";
+    moveto 0 0;
+    draw_string "When in Threshold mode, [+]/[-] to increase/decrease the threshold."
+  in
+
+  while not !exit do
+    clear_graph ();
+    bg ();
+    draw_image pict_img margin margin;
+    set_color black;
+    rect (2*margin + width) margin (128*size + 1) (64*size + 1);
+    (if !fs_mode
+      then draw_image pict_mono_fs (2*margin + width + 1) (margin + 1)
+      else
+        let pict_mono_th = make_image (mono_to_image_mat !mono_th size) in
+        draw_image pict_mono_th (2*margin + width + 1) (margin + 1));
+    set_color red;
+    (if width < height*2
+      then (* Ratio < 2:1 (portrait, 16/9...) *)
+        rect margin (margin + height - width/2) width (width/2)
+      else (* Ratio >= 2:1 (flat landscape) *)
+        rect margin margin (2*height) height);
+    synchronize ();
+
+    let {mouse_x; mouse_y; button; keypressed; key} =
+			wait_next_event [Button_down; Key_pressed]
+		in
+		exit := key = '\027'; (* Esc *)
+    if keypressed then
+      if key = 'a' then
+        fs_mode := not !fs_mode
+      else if not !fs_mode then
+        if key = '+' && !thresh <> 1. then
+          (thresh := min 1. (!thresh +. 0.1);
+          mono_th := img_to_mono_threshold rimg !thresh)
+        else if key = '-' && !thresh <> 0. then
+          (thresh := max 0. (!thresh -. 0.1);
+          mono_th := img_to_mono_threshold rimg !thresh)
+  done;
+  close_graph();
+  if !fs_mode
+    then mono_fs
+    else !mono_th;;
 
 (* Testing function *)
 let test_fs (s : string) =
   (* let img = read_bmp "/mnt/c/users/blanc/desktop/ocaml_tests/basic_project/mandelbrot_bmp.bmp" in *)
   let img = read_bmp ("/mnt/c/users/blanc/desktop/ocaml_tests/basic_project/"^s^".bmp") in
-  let mono = floyd_steinberg img in
-  view_side_by_side img mono;;
+  view_side_by_side img;;
 
 (* Todo:
-   - Take care of the threshold
+   - Debug "Graphics.Graphic_failure "Xlib error: BadPixmap (invalid Pixmap parameter)"" on penguin.
+   - Threshold looks a lot like FS......
   *)
